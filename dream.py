73433c90f5043b443326c84db8b976540e7ad6ff
Madeleine Taft <mpt37@cornell.edu>
2021-06-30 20:32:00 -0400

    edit and delete buttons

--- dream.py
+++ dream.py
@@ -4,34 +4,81 @@ from werkzeug.exceptions import abort
 
 
 def get_db_connection():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect("database.db")
     conn.row_factory = sqlite3.Row
     return conn
 
+
 def get_post(post_id):
     conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    post = conn.execute("SELECT * FROM posts WHERE id = ?", (post_id,)).fetchone()
     conn.close()
     if post is None:
         abort(404)
     return post
 
-app = Flask(__name__)
 
-@app.route('/')
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "ziopn4rlsdfu983n"
+
+
+@app.route("/")
 def home():
     conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
+    posts = conn.execute("SELECT * FROM posts").fetchall()
     conn.close()
-    return render_template('home.html', posts=posts)
+    return render_template("home.html", posts=posts)
 
-@app.route('/<int:post_id>')
+
+@app.route("/<int:post_id>")
 def post(post_id):
     post = get_post(post_id)
-    return render_template('post.html', post=post)
+    return render_template("post.html", post=post)
 
-@app.route('/create', methods=('GET', 'POST'))
+
+@app.route("/create", methods=("GET", "POST"))
 def create():
-    return render_template('create.html')
-    
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        if not title:
+            flash("Title is required!")
+        else:
+            conn = get_db_connection()
+            conn.execute(
+                "INSERT INTO posts (title, content) VALUES (?, ?)", (title, content)
+            )
+            conn.commit()
+            conn.close()
+            return redirect(url_for("home"))
 
+    return render_template("create.html")
+
+
+@app.route("/<int:post_id>/delete", methods=["POST"])
+def delete(post_id):
+    conn = get_db_connection()
+    conn.execute("DELETE FROM posts WHERE id = ? ", (post_id,))
+    conn.commit()
+    conn.close()
+    flash("Successfully deleted post!")
+
+    return redirect(url_for("home"))
+
+
+@app.route("/<int:post_id>/edit", methods=["GET", "POST"])
+def edit(post_id):
+    post = get_post(post_id)
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        conn = get_db_connection()
+        conn.execute(
+            "UPDATE posts SET (title, content) = (?, ?) WHERE id = ?",
+            (title, content, post_id),
+        )
+        conn.commit()
+        conn.close()
+        return redirect(url_for("post", post_id=post_id))
+
+    return render_template("edit.html", post=post)
